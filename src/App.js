cc8c7116f7a19a816160a9d62d956df84569c4b0
Ahmed Jehanzaib <ahmedjehanzaib@Ahmeds-MacBook-Pro.local>
2021-04-21 17:06:03 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import ReactJson from 'react-json-view';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [obj, setObj] = useState({});
+
+  const onAddEvent = (added) => {
+    setObj({ ...added.updated_src })
+  }
+
+  const onEditEvent = (edited) => {
+    setObj({ ...edited.updated_src })
+  }
+
+  const onDeleteEvent = (deleted) => {
+    setObj({ ...deleted.updated_src })
+  }
+
+
+  const saveJsonValue = () => {
+    window.alert(JSON.stringify(obj, null));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>React Json Editor/Viewer</h3>
+      <form>
+        <ReactJson
+          theme={'bright:inverted'}
+          src={obj}
+          onAdd={(add) => {
+            onAddEvent(add);
+          }}
+          onEdit={(edit) => {
+            onEditEvent(edit)
+          }}
+          onDelete={(d) => { onDeleteEvent(d) }}
+        />
+      </form>
+      <hr />
+      <button type="submit" onClick={saveJsonValue}>Save</button>
     </div>
   );
 }
